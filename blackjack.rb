4a44c7bc13c23a0f450f883221bff6f9b8425c22
Kate Walters <kawalters01@gmail.com>
2016-10-09 19:17:38 -0500

    Added Deck intialize method

--- blackjack.rb
+++ blackjack.rb
@@ -10,3 +10,15 @@ class Card
     "#{@value} of #{suit}"
   end
 end
+
+class Deck
+  def initialize
+    @cards = []
+    suits = [:diamonds, :hearts, :clubs, :spades]
+    suits.each do |suit|
+      (2..14).each do |value|
+        @cards << Card.new(suit, value)
+      end
+    end
+  end
+end
